6e48efb23b5c6e4ce4a27d39cbe4a16488444c3a
slice-mani <mani.c@sliceit.com>
2022-01-25 17:59:25 +0530

    feat(feat): implemented new feature

---
+++ src/main/java/testing/Feat1.java
@@ -0,0 +1,8 @@
+package testing;
+
+public class Feat1 {
+  public void test() {
+    System.out.println("Testing greet");
+    new Test().greet();
+  }
+}
--- src/main/java/testing/Test.java
+++ src/main/java/testing/Test.java
@@ -5,8 +5,4 @@ public class Test {
   public void greet() {
     System.out.println("Hi, Welcome");
   }
-
-  public void test() {
-    greet();
-  }
 }
